485aa83de5515ac742560346eadd80bd4d67783a
souldbminersmwc <souldbyt@gmail.com>
2026-04-02 16:51:43 -0400

    hocclk: remove sysclk licensing from hocclk code

--- Source/hoc-clk/sysmodule/src/integrations.cpp
+++ Source/hoc-clk/sysmodule/src/integrations.cpp
@@ -15,15 +15,6 @@
  *
  */
 
-/* --------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * <p-sam@d3vs.net>, <natinusala@gmail.com>, <m4x@m4xw.net>
- * wrote this file. As long as you retain this notice you can do whatever you
- * want with this stuff. If you meet any of us some day, and you think this
- * stuff is worth it, you can buy us a beer in return.  - The sys-clk authors
- * --------------------------------------------------------------------------
- */
-
 #include "integrations.hpp"
 #include <sys/stat.h>
 #include <SaltyNX.h>
