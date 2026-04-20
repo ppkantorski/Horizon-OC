3704d461366cdc4e5023340f86e768836442c318
Lightos1 <124387232+Lightos1@users.noreply.github.com>
2026-04-20 21:30:02 +0200

    set default step mode to 66MHz

--- Source/Atmosphere/stratosphere/loader/source/oc/customize.cpp
+++ Source/Atmosphere/stratosphere/loader/source/oc/customize.cpp
@@ -42,7 +42,7 @@ volatile CustomizeTable C = {
 
 /* Available: 66MHz step rate, 100MHz step rate and jedec. */
 /* Jedec freqs are 1333MHz, 1600MHz, 1866MHz, 2133MHz, 2400MHz, 2666MHz, 2933MHz, 3200MHz. */
-.stepMode = StepMode_Jedec,
+.stepMode = StepMode_66MHz,
 
 .marikoEmcMaxClock = 2133000, /* 1866MHz @ 1866tWRL is guaranteed to work on all Mariko units */
 .marikoEmcVddqVolt = 600000,
