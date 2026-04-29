352b6965d335e92084b671b5c6fa58c5ba42b387
ppkantorski <6467366+ppkantorski@users.noreply.github.com>
2026-04-29 03:16:49 -0700

    Add copyright and license information to kip.hpp
    Added copyright notice and licensing information to kip.hpp.

--- Source/sys-clk-hoc/sysmodule/src/kip.hpp
+++ Source/sys-clk-hoc/sysmodule/src/kip.hpp
@@ -5,6 +5,23 @@
  * the kip namespace.  All KIP write/validate logic has been removed.
  */
 
+/*
+ * Copyright (c) Souldbminer, Lightos_ and Horizon OC Contributors
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms and conditions of the GNU General Public License,
+ * version 2, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #pragma once
 #include <stdbool.h>
 
